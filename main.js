2d4151a79696b7e5505de52b7c44de10fd0f8005
Elizabeth Andoun <elizabethandoun@Sts-iMac.attlocal.net>
2021-01-21 15:23:59 -0600

    final commit

--- main.js
+++ main.js
@@ -1,25 +1,74 @@
 //console.log("Eliazbeth")
 let dieRolls=[];
 let userroll=document.querySelector("#dice-amount");
-let roll=document.querySelector("#roll-button");
-let rollcount=document.querySelector("#total-rolls");
-let rollreset=document.querySelector("#reset-button");
-let revealdice=document.querySelector("showallrolls");
+let rollButton=document.querySelector("#roll-button");
+let totalSpan=document.querySelector("#total");
+let resetButton=document.querySelector("#reset-button");
+let showallrolls=document.querySelector("#showallrolls");
+let allRollsElement=document.querySelector("#allRolls")
 
 
-roll.addEventListener("click", function (){
+rollButton.addEventListener("click", function (){
 let diceCount = userroll.value;
-console.log (diceCount)
+// console.log (diceCount)
+let total = 0;
+for(let count =1;count<=diceCount;  count++ ){
+
+ let roll = (Math.floor( Math.random() * 6)+ 1)
+dieRolls.push(roll)
+total = total + roll;
+ }
+ totalSpan.innerHTML = total
 })
 
 //  const list = document.querySelector('#"Allrolls"');
 //  list.innerHTML='';
  
-var numbers = [1,2,3,4,5,6];
-for (var i = 0; i < numbers.length; i++) {
-    console.log(numbers[i]);
-}
+
+showallrolls.addEventListener("click", function(){
+    for(let count = 0; count < dieRolls.length; count++){
  
+         const newDiceString = '<li class ="dice">' + dieRolls[count] + "</li>";
+         allRollsElement.innerHTML = allRollsElement.innerHTML + newDiceString;
+    }
+})
+let diceReset = document.querySelector("#reset-button")
+
+
+diceReset.addEventListener("click",function (){
+    
+    newValue = 0;
+    totalSpan.innerHTML = newValue;
+    newValue = 0;
+    allRollsElement.innerHTML = newValue;
+    newValue = 0;
+    userroll.innerHTML = newValue;
+    newValue = 0;
+    
+
+})
+    // let count = 0;
+    // totalRolls.innerHTML ="";
+    
+    // while (count < dieRolls.length){
+    //     showAllRolls.innerHTML += '<li>' + (count +1)+ ''
+    //     count++;
+  
+    
+
+// alert('You rolled a ' + dieRoll);
+
+//  resetButton.addEventListener("click", function () {
+//     newValue = Number(resetButton.innerHTML) = 0 ;
+//     reset.innerHTML = newValue;
+//     newValue = 0;
+//     dieroll.innerHTML = newValue;
+//     newValue = 0;
+//     totalRolls.innerHTML = newValue;
+//     newValue = 0;
+//     showAllRolls.innerHTML = newValue;
+//     newValue = 0;
+// })
 // for (int Allrolls = 1; Allrolls <= 10; Allrolls++)
 //        {
 //         int Random = (int)(Math.random()*100);
@@ -45,12 +94,3 @@ for (var i = 0; i < numbers.length; i++) {
     
 // })
 
-// rollreset.addEventListener("click", function () {
-//     newValue = Number(reset.innerHTML) + 1;
-//     reset.innerHTML = newValue;
-//     newValue = 0;
-//     userroll.innerHTML = newValue;
-//     newValue = 0;
-//     rollcount.innerHTML = newValue;
-//     newValue = 0;
-// })
